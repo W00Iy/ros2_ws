9f6a0ff5173514febb8c4fa37c463dcb2cbe61d6
W00Iy <benjambd@stud.ntnu.no>
2026-03-03 10:21:28 +0100

    Funny  Test

--- src/joint_description/setup.py
+++ src/joint_description/setup.py
@@ -11,12 +11,12 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-    ('share/ament_index/resource_index/packages',
-        ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/joint_description']),
+        ('share/joint_description', ['package.xml']),
+        ('share/joint_description/urdf', ['urdf/joint_model.urdf']),
+        ('share/joint_description/launch', ['launch/view_model.launch.py']),
     ],
-
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='wooly',
